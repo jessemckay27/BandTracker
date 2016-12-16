317b0493a94e0e3d56d22cae59f767ff76c07ad8
Jesse <rosecity27@comcast.net>
2016-12-16 12:57:30 -0800

    adds Save method for Band.cs

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -87,6 +87,39 @@ namespace BandTracker.Objects
       return allBands;
     }
 
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO bands (name) OUTPUT INSERTED.id VALUES (@BandName);", conn);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@BandName";
+      nameParameter.Value = this.GetName();
+
+      cmd.Parameters.Add(nameParameter);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+
+
+
+
     public static void DeleteAll()
     {
       SqlConnection conn = DB.Connection();
