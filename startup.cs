963ce4450b26d292ece3905b4a973f1d1d2ce834
Jesse <rosecity27@comcast.net>
2016-12-16 09:05:03 -0800

    initial commit

---
+++ startup.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.IO;
+using Nancy;
+using Nancy.Owin;
+using Nancy.ViewEngines.Razorusing
+using Microsoft.AspNet.Builder;
+
+namespace BandTracker
+{
+  public class Startup
+  {
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseOwin(x => x.UseNancy());
+    }
+  }
+}
+
+public class CustomRootPathProvider : IRootPathProvider
+ {
+   public string GetRootPath()
+   {
+     return Directory.GetCurrentDirectory();
+   }
+ }
+
+ public class RazorConfig : IRazorConfiguration
+ {
+   public IEnumerable<string> GetAssemblyNames()
+   {
+     return null;
+   }
+
+   public IEnumerable<string> GetDefaultNamespaces()
+   {
+     return null;
+   }
+
+   public bool AutoIncludeModelNamespace
+   {
+     get { return false; }
+   }
+ }
+
+ public static class DBConfiguration
+ {
+    //  public static string ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=registrar;Integrated Security=SSPI;";
+     public static string ConnectionString = "Data Source=desktop-ddsnb9e;Initial Catalog=registrar;Integrated Security=SSPI;";
+ }
+}
